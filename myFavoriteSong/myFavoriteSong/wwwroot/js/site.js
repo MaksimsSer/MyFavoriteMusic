ccc7735148a6e15a9f9101a8f157d8975586d1b6
Maksims Sersnovs <maksimssir@gmail.com>
2023-11-21 21:04:34 +0000

    Firstproject
    myfavoritesong

---
+++ myFavoriteSong/myFavoriteSong/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// JavaScript animation
+function animateButton() {
+    const btn = document.querySelector('.play-btn');
+    btn.style.transform = 'rotate(360deg)';
+    btn.style.transition = 'transform 1s ease-in-out';
+}
